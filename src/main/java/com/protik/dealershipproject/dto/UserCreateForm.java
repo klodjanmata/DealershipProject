d286151d36df382eb36db598ce1c5c87b27f9dd2
Klodjan Mata <klodjanmata0@gmail.com>
2025-11-09 12:02:33 +0100

    Changes

--- src/main/java/com/protik/dealershipproject/dto/UserCreateForm.java
+++ src/main/java/com/protik/dealershipproject/dto/UserCreateForm.java
@@ -1,7 +1,11 @@
 package com.protik.dealershipproject.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserCreateForm {
 
     @NotBlank(message = "Username is required")
@@ -13,27 +17,4 @@ public class UserCreateForm {
     @NotBlank(message = "Role is required")
     private String role;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
